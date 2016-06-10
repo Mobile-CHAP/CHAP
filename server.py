d18e3a6ea174228ed96952acb9cceddba7c0ee16
dominic.cassidy93@gmail.com <dominic.cassidy93@gmail.com>
2016-06-10 11:29:29 +0100

    Multple clients now possible.

--- server.py
+++ server.py
@@ -17,15 +17,20 @@ def root():
 def video_feed():
     def run_camera():
         while True:
+            # Give server enough time to listen for other requests.
+            #gevent.sleep(0.017) # 60fps
+            #gevent.sleep(0.02) # 50fps
+            gevent.sleep(0.033) # 30fps
+            #gevent.sleep(0.042)  # 24fps
             frame = camera.get_frame()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     return Response(stream_with_context(run_camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def run_server():
-    print "Server running on port 80"
+    print "Server running on port 8080"
     pool = Pool(5)
-    http_server = WSGIServer(('', 80), app,spawn=pool)
+    http_server = WSGIServer(('', 8080), app,spawn=pool)
     http_server.serve_forever()
     
 if __name__ == "__main__":
