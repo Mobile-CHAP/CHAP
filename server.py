bd646120d54447a226e9c561b9f488bfe12b670e
dominic.cassidy93@gmail.com <dominic.cassidy93@gmail.com>
2016-06-10 09:46:28 +0100

    Improved stability - made camera a global variable.

--- server.py
+++ server.py
@@ -1,20 +1,32 @@
-from flask import Flask, render_template, Response
+from flask import Flask, stream_with_context, render_template, Response
 from camera import Camera
+
+import gevent.monkey
+from gevent.wsgi import WSGIServer
+from gevent.pool import Pool
+
+gevent.monkey.patch_all()
 app = Flask(__name__, static_url_path='/static')
+camera = Camera()
 
 @app.route("/")
 def root():
     return render_template("index.html", title = 'Controller')
-    
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    def run_camera():
+        while True:
+            frame = camera.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    return Response(stream_with_context(run_camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+def run_server():
+    print "Server running on port 80"
+    pool = Pool(5)
+    http_server = WSGIServer(('', 80), app,spawn=pool)
+    http_server.serve_forever()
+    
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    run_server()
